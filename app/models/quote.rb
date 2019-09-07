687e518f189a492af7872f278b48817a7195fa94
Tiffany Curran <tcurran101@gmail.com>
2019-09-07 16:34:53 +0000

    fixed bugs. added validation for author on quote form

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,4 @@
 class Quote < ApplicationRecord
   validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :author, presence: true, length: { maxium: 50, minimum: 3 }
 end
