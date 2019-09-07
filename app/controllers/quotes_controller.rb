1a85648519d6a9eb18a3ac5db4a366f2aec07e92
Tiffany Curran <tcurran101@gmail.com>
2019-09-07 15:55:01 +0000

    redid create quote code

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,9 +7,13 @@ class QuotesController < ApplicationController
     @quote = Quote.new
   end
 
-  def create
-    Quote.create(quote_params)
+ def create
+  @quote = Quote.create(quote_params)
+  if @quote.invalid?
+    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
   end
+  redirect_to root_path
+end
 
   private
 
